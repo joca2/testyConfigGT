5ed22d6b379a18a93ba69adde2a70f23d55d2dd6
joca2 <joca2@wp.pl>
2018-02-07 14:56:27 +0100

    kolejna poprawka gitignore

---
+++ autoload-x/app.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+wywali komentarz ???
+*/
+
+/*
+ * wywali komentarz ???
+ */
+
+/**
+ * wywali komentarz ???
+ */
+
+return array(
+    'app_options' => array(
+        'dev' => DEV,
+
+        'view_helper_enable' => [
+            'AppPhotoHeader' => 0,
+            'jcCarousel' => 0,
+        ],
+
+
+        'session' => [
+            'name'                => 'T_SESSID',
+            'save_path'           => "./data/session",
+            'remember_me_seconds' => null,
+            'cookie_lifetime'     => null,
+            'expiration'          => null,
+        ]
+    ),
+
+
+
+);
