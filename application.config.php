66d5b55180db1be75724f8660358691e1c2cbc17
joca2 <joca2@wp.pl>
2018-02-09 11:04:54 +0100

    poprawki online

--- application.config.php
+++ application.config.php
@@ -1,45 +1,16 @@
 <?php
 
-
 $modules = array();
 
-$modules[] = 'jc';
-
 if (DEV) {
-    //$modules[] = 'ZendDeveloperTools';
-    //$modules[] = 'SanSessionToolbar';
     $modules[] = 'Dev';
 }
 
-
-$modules[] = 'JcMsieRedirect';
-
-//$modules[] = 'JcCache';
-
-$modules[] = 'JcErrorHandler';
-
-$modules[] = 'ZfcBase';
-$modules[] = 'ZfcUser';
-$modules[] = 'JHttps';
-$modules[] = 'MtMail';
-
-$modules[] = 'ZfOpenGraph';
 $modules[] = 'Application';
 $modules[] = 'JcNavigation';
 
-$modules[] = 'JcInfo';
-$modules[] = 'JcImages';
-
-$modules[] = 'JcFirma';
-
-$modules[] = 'JcView';
-
-$modules[] = 'JcTest';
-
-
 return array(
     'modules' => $modules,
-
     'module_listener_options' => array(
         'module_paths' => array(
             './module',
